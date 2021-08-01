041ca5c413891dac8a30ddd6ff7eafc221867fca
Ehsan Mozaffari <ehsan.mozaffari@yahoo.com>
2021-08-01 21:51:51 +0430

    add http4s routes

--- src/main/scala/com/example/Main.scala
+++ src/main/scala/com/example/Main.scala
@@ -1,5 +1,8 @@
 package com.example
 
-object Main extends App {
-  println("Hello world scala FP!")
+import cats.effect.{ExitCode, IO, IOApp}
+
+object Main extends IOApp {
+  def run(args: List[String]): IO[ExitCode] =
+    Http4sServer.stream[IO].compile.drain.as(ExitCode.Success)
 }
